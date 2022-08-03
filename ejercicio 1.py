fbc598b9580242b0b6956ef28248a068cb051341
Ro0z <roz.unlp@gmail.com>
2022-08-03 10:42:37 -0300

    Update ejercicio 1.py

--- ejercicio 1.py
+++ ejercicio 1.py
@@ -1,4 +1,4 @@
-print ("Ingrese la velocidad y el tiempo de la unidad movil")
+print("Ingrese la velocidad y el tiempo de la unidad movil")
 V = float( input("Velocidad: ") )
 T = int( input("Tiempo: ") )
 D = V*T
