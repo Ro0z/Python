c9c5f8b294436ee8ec22ea43bf7bb86ce62ecf1e
Ro0z <roz.unlp@gmail.com>
2022-08-05 11:23:41 -0300

    HOLA

---
+++ ejercicio 11.py
@@ -0,0 +1,13 @@
+PI = 3.1416
+print("Ingrese Radio y Alto: ") 
+radio = float( input("Radio: ")) 
+alto = float ( input("Alto: "))
+vol = PI * radio*2 * alto
+are = 2*PI*radio*(radio + alto)
+print("\nSALIDA: ") 
+
+print("") 
+print("Volumen:", vol)
+print("area:", are)
+
+
